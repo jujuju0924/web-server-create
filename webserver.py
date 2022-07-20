d0a028ffcb391255031f9d688de5a3646620e71f
jumpei arai <jumpei0924@gmail.com>
2022-07-20 23:55:38 +0900

    update

--- webserver.py
+++ webserver.py
@@ -1,8 +1,9 @@
 import socket
+from datetime import datetime
 
-class TCPServer:
+class WebServer:
   """
-  TCP通信を行うサーバーを表すクラス
+  Webサーバーを表すクラス
   """
   def serve(self):
     """
@@ -32,9 +33,22 @@ class TCPServer:
       with open("server_recv.txt","wb") as f:
         f.write(request)
 
-      # クライアントへ送信するレスポンスデータをファイルから取得する
-      with open("server_send.txt","rb") as f:
-        response = f.read()
+      # レスポンスボディを生成
+      response_body = "<html><body><h1>It works!</h1></body></html>"
+
+      # レスポンスラインを生成
+      response_line = "HTTP/1.1 200 OK\r\n"
+
+      # レスポンスヘッダーを生成
+      response_header = ""
+      response_header += f"Date: {datetime.utcnow().strftime('%a, %d %b %Y %H:%M:%S GMT')}\r\n"
+      response_header += "Host: HenaServer/0.1\r\n"
+      response_header += f"Content-Length: {len(response_body.encode())}\r\n"
+      response_header += "Connection: Close\r\n"
+      response_header += "Content-Type: text/html\r\n"
+
+      # ヘッダーとボディを空行でくっつけた上でbytesに変換し、レスポンス全体を生成する
+      response = (response_line + response_header + "\r\n" + response_body).encode()
 
       # クライアントへレスポンスを送信する
       client_socket.send(response)
@@ -46,5 +60,5 @@ class TCPServer:
         print("=== サーバを停止します。 ===")
 
 if __name__ == "__main__":
-    server = TCPServer()
+    server = WebServer()
     server.serve()
